ff024cd52b82124269e9def1360fcf5fd17b2519
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-09-10 19:37:15 +0500

    Updates

---
+++ precache-manifest.eb8a15dac5bbe4f63de805fbab461906.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a92106dc2ac2e62c169dba107e39fe",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "bd2f65fac2cfa53c04ff",
+    "url": "/MyPortfolio/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "0438cf8a044496cdac66",
+    "url": "/MyPortfolio/static/css/main.32fed52b.chunk.css"
+  },
+  {
+    "revision": "bd2f65fac2cfa53c04ff",
+    "url": "/MyPortfolio/static/js/2.1be19a78.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/MyPortfolio/static/js/2.1be19a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0438cf8a044496cdac66",
+    "url": "/MyPortfolio/static/js/main.dff32953.chunk.js"
+  },
+  {
+    "revision": "8e14a5e44e013bce72b0",
+    "url": "/MyPortfolio/static/js/runtime-main.4d80552a.js"
+  },
+  {
+    "revision": "b0f89786b875a1517205bd8493283ab5",
+    "url": "/MyPortfolio/static/media/ChristianMusic.b0f89786.png"
+  },
+  {
+    "revision": "52b925f766f5be2887bc0e19be859d21",
+    "url": "/MyPortfolio/static/media/burger.52b925f7.png"
+  },
+  {
+    "revision": "5cd1aa0951ee6a74742b6eefd3c001ec",
+    "url": "/MyPortfolio/static/media/clothing.5cd1aa09.png"
+  },
+  {
+    "revision": "6f4458c3ed522de639d9430c25f4d984",
+    "url": "/MyPortfolio/static/media/corona.6f4458c3.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/MyPortfolio/static/media/man.52be3144.svg"
+  },
+  {
+    "revision": "6d6ed4378e42c931ddec90ff33dd3ad2",
+    "url": "/MyPortfolio/static/media/medicure.6d6ed437.png"
+  },
+  {
+    "revision": "b6fb55d66be4f5b8e4a448de5bcdae48",
+    "url": "/MyPortfolio/static/media/mob.b6fb55d6.png"
+  },
+  {
+    "revision": "0e62cbf07409db9ace05f0e8656af321",
+    "url": "/MyPortfolio/static/media/nike.0e62cbf0.png"
+  },
+  {
+    "revision": "e9520ac77ce39683185b5e15943019b8",
+    "url": "/MyPortfolio/static/media/pic.e9520ac7.jpg"
+  },
+  {
+    "revision": "b915ac8dc5fda9ec555235565d7d631a",
+    "url": "/MyPortfolio/static/media/resume.b915ac8d.pdf"
+  },
+  {
+    "revision": "4ec81d95f0bfa4741b9a78f65568a572",
+    "url": "/MyPortfolio/static/media/vision.4ec81d95.png"
+  }
+]);
